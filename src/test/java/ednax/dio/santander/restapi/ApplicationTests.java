0b7b2042e9ffc56e86fa887ccef27bfe93a612dd
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-09-20 21:40:40 -0300

    First commit

---
+++ src/test/java/ednax/dio/santander/restapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ednax.dio.santander.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
