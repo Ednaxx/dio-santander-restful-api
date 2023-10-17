756fc485e4cad033246163850e1c36b084651840
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-10-17 20:14:43 -0300

    Implementing WorkoutProgramService Tests

--- src/test/java/ednax/dio/santander/restapi/services/WorkoutProgramServiceTests.java
+++ src/test/java/ednax/dio/santander/restapi/services/WorkoutProgramServiceTests.java
@@ -1,6 +1,11 @@
 package ednax.dio.santander.restapi.services;
 
+import ednax.dio.santander.restapi.dtos.request.WorkoutProgramRequestDTO;
+import ednax.dio.santander.restapi.dtos.response.WorkoutProgramResponseDTO;
 import ednax.dio.santander.restapi.exceptions.RestException;
+import ednax.dio.santander.restapi.models.TeacherModel;
+import ednax.dio.santander.restapi.models.UserModel;
+import ednax.dio.santander.restapi.models.WorkoutProgramModel;
 import ednax.dio.santander.restapi.repositories.TeacherRepository;
 import ednax.dio.santander.restapi.repositories.UserRepository;
 import ednax.dio.santander.restapi.repositories.WorkoutProgramRepository;
@@ -10,9 +15,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 public class WorkoutProgramServiceTests {
@@ -25,19 +38,42 @@ public class WorkoutProgramServiceTests {
     private TeacherRepository teacherRepository;
     @Mock
     private UserRepository userRepository;
-
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private SecurityContext securityContext;
     @Spy
     private ModelMapper modelMapper;
 
 
     @Test
     public void shouldCreateWorkoutProgram() {
+        Mockito.when(workoutProgramRepository.save(Mockito.any(WorkoutProgramModel.class))).thenReturn(new WorkoutProgramModel());
+        Mockito.when(userRepository.findById(Mockito.any(UUID.class))).thenReturn(Optional.of(new UserModel()));
 
+        Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        Mockito.when(teacherRepository.findByLogin(Mockito.any())).thenReturn(new TeacherModel());
+
+        WorkoutProgramResponseDTO expected = new WorkoutProgramResponseDTO();
+
+        WorkoutProgramRequestDTO request = new WorkoutProgramRequestDTO();
+        request.setUser(UUID.randomUUID().toString());
+
+        Assertions.assertEquals(expected, workoutProgramService.create(request));
     }
 
     @Test
     public void shouldThrowExceptionWhenProgramsUserDoesNotExists_onCreateWorkoutProgram() {
+        var uuid = UUID.randomUUID();
 
+        Mockito.when(userRepository.findById(uuid)).thenReturn(Optional.empty());
+
+        WorkoutProgramRequestDTO request = new WorkoutProgramRequestDTO();
+        request.setUser(uuid.toString());
+
+        Assertions.assertThrows(RestException.class, () -> workoutProgramService.create(request));
     }
 
 
