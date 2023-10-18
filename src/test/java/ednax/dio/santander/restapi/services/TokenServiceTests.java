b1b480f0b68a9cd122f689bc475e173cfb2d3467
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-10-18 13:42:48 -0300

    Fixed TokenService @Value null issue

---
+++ src/test/java/ednax/dio/santander/restapi/services/TokenServiceTests.java
@@ -0,0 +1,10 @@
+package ednax.dio.santander.restapi.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TokenServiceTests {
+
+
+}
