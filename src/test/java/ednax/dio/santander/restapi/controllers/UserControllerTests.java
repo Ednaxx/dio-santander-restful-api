66c4ce93b91a8c9588b14ee0f2e50dabdfd8a6a0
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-10-20 13:44:00 -0300

    Added UserController tests

--- src/test/java/ednax/dio/santander/restapi/controllers/UserControllerTests.java
+++ src/test/java/ednax/dio/santander/restapi/controllers/UserControllerTests.java
@@ -1,10 +1,18 @@
 package ednax.dio.santander.restapi.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import ednax.dio.santander.restapi.dtos.request.UserRequestDTO;
 import ednax.dio.santander.restapi.dtos.response.UserResponseDTO;
+import ednax.dio.santander.restapi.dtos.response.WorkoutProgramResponseDTO;
+import ednax.dio.santander.restapi.models.UserModel;
 import ednax.dio.santander.restapi.services.implementations.UserServiceImpl;
+import net.minidev.json.parser.JSONParser;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,7 +24,10 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(UserController.class)
@@ -28,6 +39,8 @@ public class UserControllerTests {
 
     @MockBean
     private UserServiceImpl userService;
+
+    private final ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
     @Test
     public void getAll() throws Exception {
@@ -43,6 +56,115 @@ public class UserControllerTests {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn();
+
+    }
+
+    @Test
+    public void getBtId() throws Exception {
+        var id = UUID.randomUUID();
+        Mockito.when(userService.findById(id.toString())).thenReturn(new UserResponseDTO());
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get(String.format("/users/%s", id.toString()))
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+    @Test
+    public void create() throws Exception {
+        Mockito.when(userService.create(Mockito.any(UserRequestDTO.class)))
+                .thenReturn(new UserResponseDTO());
+
+        UserRequestDTO userRequest = new UserRequestDTO(
+                "login",
+                "firstName",
+                "surname",
+                "male",
+                new Date(),
+                BigDecimal.ONE,
+                1);
+        String jsonRequest = objectWriter.writeValueAsString(userRequest);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/users")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(jsonRequest);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+        var id = UUID.randomUUID();
+        Mockito.doNothing().when(userService).delete(id.toString());
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete(String.format("/users/%s", id.toString()))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void update() throws Exception {
+        var id = UUID.randomUUID();
+
+        UserRequestDTO userRequest = new UserRequestDTO(
+                "login",
+                "firstName",
+                "surname",
+                "male",
+                new Date(),
+                BigDecimal.ONE,
+                1);
+        String jsonRequest = objectWriter.writeValueAsString(userRequest);
+
+        Mockito.when(userService.update(id.toString(), userRequest))
+                .thenReturn(new UserResponseDTO());
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .put(String.format("/users/%s", id.toString()))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8")
+                .content(jsonRequest);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+    }
+
+    @Test
+    public void getUsersWorkoutPrograms() throws Exception {
+        var id = UUID.randomUUID();
+
+        Mockito.when(userService.findUsersWorkoutPrograms(id.toString()))
+                .thenReturn(List.of(new WorkoutProgramResponseDTO()));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get(String.format("/users/%s/workout-programs", id.toString()))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
 
     }
 
