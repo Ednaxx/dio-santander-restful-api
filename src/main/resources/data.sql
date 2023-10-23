9bd0e16df263283630d1e0057d8fc6b297dc774a
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-10-23 11:57:23 -0300

    Set development profile to run on H2 and auto create a root teacher entity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into teacher (id, first_name, login, surname, password) values ('81104376-c971-4700-9548-12850b127709', 'root', 'root', 'root', '$2a$10$mmB5dqWH05tmPa9DOnD8BuJ.osR5w8SFm.s6ssiYzzHa9WObSffxC');
