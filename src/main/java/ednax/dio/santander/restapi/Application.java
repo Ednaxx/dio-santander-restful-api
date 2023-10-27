2774f14f12fe8af419f44d5685145e8bf9e7cbb6
Alexandre Morais <xandre2setembro2002@hotmail.com>
2023-10-26 23:32:14 -0300

    Added simple rate-limiting

--- src/main/java/ednax/dio/santander/restapi/Application.java
+++ src/main/java/ednax/dio/santander/restapi/Application.java
@@ -2,8 +2,10 @@ package ednax.dio.santander.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
